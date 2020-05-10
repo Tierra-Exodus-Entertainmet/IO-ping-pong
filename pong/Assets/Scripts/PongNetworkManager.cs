acae7145d9f57840359f925b7a8af233d6132598
SaiVivek Amirishetty <vivekboss99@gmail.com>
2020-05-10 10:26:59 +0530

    Added Game Data

---
+++ pong/Assets/Scripts/PongNetworkManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PongNetworkManager : NetworkManager
+{
+    public Transform leftRacketSpawn;
+    public Transform rightRacketSpawn;
+    GameObject ball;
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        //  base.OnServerAddPlayer(conn);
+        Transform start = numPlayers == 0 ? leftRacketSpawn : rightRacketSpawn;
+        GameObject player = Instantiate(playerPrefab, start.position, start.rotation);
+        NetworkServer.AddPlayerForConnection(conn, player);
+
+        if (numPlayers == 2)
+        {
+            ball = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "Ball"));
+            NetworkServer.Spawn(ball);
+        }
+    }
+
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        //base.OnServerDisconnect(conn);
+        if (ball != null)
+        {
+            NetworkServer.Destroy(ball);
+        }
+
+        base.OnServerDisconnect(conn);
+    }
+
+}
+
---
+++ pong/Assets/Scripts/RacketMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class RacketMovement : NetworkBehaviour
+{
+    public float speed = 1500;
+    public Rigidbody2D Rigidbody2d;
+
+
+    void FixedUpdate()
+    {
+        // only let the local player control the racket.
+        // don't control other player's rackets
+        if (isLocalPlayer)
+            Rigidbody2d.velocity = new Vector2(0, Input.GetAxisRaw("Vertical")) * speed * Time.fixedDeltaTime;
+    }
+
+}
